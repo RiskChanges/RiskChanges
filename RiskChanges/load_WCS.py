948491c42550429c3a66a817c1f7d5a4ed6cdd35
iamtekson <teksondada@gmail.com>
2021-08-09 19:58:38 +0700

    load_WCS correction

--- RiskChanges/load_WCS.py
+++ RiskChanges/load_WCS.py
@@ -1,28 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-
-# Load WCS Module
-
-
-# In[3]:
-
-
-from osgeo import gdal
-import rasterio
-import numpy
-import ogr
-import os
-import sys
-from rasterstats import zonal_stats
-from shapely.geometry import LineString
-from shapely import wkt
 from owslib.wcs import WebCoverageService
-
-
-# In[18]:
 
 
 def loadWCS(folder, out_name, WCS_URL, Version, layerName, bbox, srid):
@@ -51,14 +28,3 @@ def loadWCS(folder, out_name, WCS_URL, Version, layerName, bbox, srid):
     temp_raster = folder+'//'+out_name+'.tif'
     with open(temp_raster, 'wb') as file:
         file.write(response.read())
-
-
-# In[19]:
-
-
-#x, y = 174100, 444100
-#bbox = (x-500, y-500, x+500, y+500)
-# loadWCS(folder="D:/",out_name='ashok_WCS',WCS_URL='http://geodata.nationaalgeoregister.nl/ahn2/wcs?service=WCS',Version='1.0.0',layerName='ahn2_05m_ruw',bbox=bbox,srid=28992)
-
-
-# In[1]:
