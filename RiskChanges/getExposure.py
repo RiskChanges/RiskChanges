29f837524ae297cd5eb39b5ff522c9cce92169b1
iamtekson <teksondada@gmail.com>
2022-05-17 11:11:08 +0700

    close #72

--- RiskChanges/getExposure.py
+++ RiskChanges/getExposure.py
@@ -5,54 +5,60 @@ import numpy as np
 from .RiskChangesOps.readvulnerability import readIntVuln, readSusVuln
 from .RiskChangesOps import readmeta, readvector, writevector, AggregateData as aggregator
 
-def getSummary(con,exposureid,agg=False):
+
+def getSummary(con, exposureid, agg=False):
     metadata = readmeta.computeloss_meta(con, exposureid)
     exposure = readvector.prepareExposureForLoss(con, exposureid)
-    hazid=metadata.hazid
-    classificationScheme=readmeta.classificationscheme(con,hazid)
+    hazid = metadata['hazid']
+    classificationScheme = readmeta.classificationscheme(con, hazid)
     base = float(metadata["base"])
     maxval = float(metadata["threshold"])
-    type_col=metadata["TypeColumn"]
+    type_col = metadata["TypeColumn"]
     stepsize = float(metadata["stepsize"])
-    min_thresholds=np.arange(start=base,stop=maxval+stepsize,step=stepsize).tolist()
-    convert_dict={}
+    min_thresholds = np.arange(
+        start=base, stop=maxval, step=stepsize).tolist()
+    convert_dict = {}
     for i in min_thresholds:
-        name=classificationScheme.query(f'val1 == {i}')['class_name'].to_list()[0]
-        convert_dict[str(i)]=name
+        name = classificationScheme.query(f'val1 == "{i}"')
+        name = name['class_name'].to_list()[0]
+        convert_dict[str(i)] = name
+
     exposure = exposure.astype({"class": str})
     exposure["class"].replace(convert_dict, inplace=True)
     if not agg:
-        summary=pd.pivot_table(exposure, values='areaOrLen', index=[type_col],
-                        columns=["class"], aggfunc=np.sum, fill_value=0)
+        summary = pd.pivot_table(exposure, values='areaOrLen', index=[type_col],
+                                 columns=["class"], aggfunc=np.sum, fill_value=0)
     else:
-        summary=pd.pivot_table(exposure, values='exposed_areaOrLen', index=[type_col],
-                        columns=["class"], aggfunc=np.sum, fill_value=0)        
+        summary = pd.pivot_table(exposure, values='exposed_areaOrLen', index=[type_col],
+                                 columns=["class"], aggfunc=np.sum, fill_value=0)
     return summary
 
 
-def getShapefile(con,exposureid,agg=False):
+def getShapefile(con, exposureid, agg=False):
     metadata = readmeta.computeloss_meta(con, exposureid)
     exposure = readvector.prepareExposureForLoss(con, exposureid)
-    hazid=metadata.hazid
-    classificationScheme=readmeta.classificationscheme(con,hazid)
+    hazid = metadata.hazid
+    classificationScheme = readmeta.classificationscheme(con, hazid)
     base = float(metadata["base"])
     maxval = float(metadata["threshold"])
-    type_col=metadata["TypeColumn"]
-    earpk=metadata["earPK"]
-    earid=metadata['earID']
+    type_col = metadata["TypeColumn"]
+    earpk = metadata["earPK"]
+    earid = metadata['earID']
     stepsize = float(metadata["stepsize"])
-    min_thresholds=np.arange(start=base,stop=maxval+stepsize,step=stepsize).tolist()
-    convert_dict={}
+    min_thresholds = np.arange(
+        start=base, stop=maxval+stepsize, step=stepsize).tolist()
+    convert_dict = {}
     for i in min_thresholds:
-        name=classificationScheme.query(f'val1 == {i}')['class_name'].to_list()[0]
-        convert_dict[str(i)]=name
+        name = classificationScheme.query(f'val1 == {i}')[
+            'class_name'].to_list()[0]
+        convert_dict[str(i)] = name
     exposure = exposure.astype({"class": str})
     exposure["class"].replace(convert_dict, inplace=True)
     if not agg:
-        summary=pd.pivot_table(exposure, values='exposed', index=['geom_id'],
-                        columns=["class"], aggfunc=np.sum, fill_value=0)
+        summary = pd.pivot_table(exposure, values='exposed', index=['geom_id'],
+                                 columns=["class"], aggfunc=np.sum, fill_value=0)
     ear = readvector.readear(con, earid)
     summary = pd.merge(left=summary, right=ear[[
-                    earpk, 'geom']], left_on='geom_id', right_on=earpk, right_index=False)
-    summary = gpd.GeoDataFrame(summary, geometry='geom')                    
+        earpk, 'geom']], left_on='geom_id', right_on=earpk, right_index=False)
+    summary = gpd.GeoDataFrame(summary, geometry='geom')
     return summary
