fix: multi-hazard risk computation

RiskChanges/ComputeMultiHazardRisk.py
@@ -24,7 +24,6 @@ def checkUniqueHazard(con, lossids):
 
 
 def predict_loss(prepared_loss, rps, probs, extensions, hazard):
-    # print(probs,"probs")
     #Lmin_x = extensions['left'][0][0]
     #Lmin_y = extensions['left'][0][1]
     #Rmin_x = extensions['right'][0][0]
@@ -43,10 +42,10 @@ def predict_loss(prepared_loss, rps, probs, extensions, hazard):
     return_periods[-1] = max_rp
     # low_rp = 'loss_rp_'+str(rps.min())
     # high_rp = 'loss_rp_'+str(rps.max())
-    low_rp = 'loss_rp_'+str(min(rps))
-    high_rp = 'loss_rp_'+str(max(rps))
-    colname_min = 'loss_rp_'+str(min_rp)
-    colname_max = 'loss_rp_'+str(max_rp)
+    low_rp = 'loss_rp_'+str(int(min(rps)))
+    high_rp = 'loss_rp_'+str(int(max(rps)))
+    colname_min = 'loss_rp_'+str(int(min_rp))
+    colname_max = 'loss_rp_'+str(int(max_rp))
     left_dx = abs(min(probs)-min_rp)
     right_dx = abs(max(probs)-min_rp)
     # left_dx = abs(probs.min()-min_rp)
@@ -65,16 +64,16 @@ def predict_loss(prepared_loss, rps, probs, extensions, hazard):
     # probs.insert(-1, Rmax_x) ##verify
     probs.insert(0, m_left)  ##verify
     probs.insert(-1, m_right) ##verify
-    print(probs,"mmmmmmmmmmmmmmmmmmmmmmmmm")
-    interp_cols = ['loss_rp_'+str(x) for x in rps]
+    interp_cols = ['loss_rp_'+str(int(x)) for x in rps]
     interp_cols.insert(-1, colname_min)
     interp_cols.insert(0, colname_max)
     interp_probs = probs
 
     predict_probs = 1/np.array(return_periods, dtype=int)
-    new_cols = [hazard+'_lrp_'+str(x) for x in return_periods]
+    new_cols = [hazard+'_lrp_'+str(int(x)) for x in return_periods]
     new_cols.append('Unit_ID')
     new_losses = []
+    # print(prepared_loss.columns,"columnsssssssssssssssssssssss")
     for index, row in prepared_loss.iterrows():
         # y = row[interp_cols].values
         y = (row[interp_cols].values).tolist()
@@ -92,11 +91,11 @@ def predict_loss(prepared_loss, rps, probs, extensions, hazard):
 
 def getMaxcost(con, lossid):
     meta = readmeta.readLossMeta(con, lossid)
+    # print(meta.columns,"metaaaaaaaaaa")
     ear_id = meta['ear_index_id'][0]
     earmeta = readmeta.earmeta(con, ear_id)
-    # earmeta = readmeta.exposuremeta(con, ear_id)
     earPK = earmeta.data_id[0]
-    cost_column = meta['ear_cost_column'][0]
+    cost_column = earmeta['col_value_avg'][0]
     cost_data = readvector.readear(con, ear_id)
     return cost_data, cost_column, earPK
 
@@ -105,11 +104,17 @@ def PrepareLossForRisk(con, lossids, extensions, hazard):
     i = True
     rps = []
     probs = []
-    for id in lossids:
+    lossids_list=[]
+    if isinstance(lossids, int):
+        lossids_list.append(lossids)
+    else:
+        lossids_list=lossids
+        
+    for id in lossids_list:
         lossdata = readvector.readLoss(con, id)
         return_period = float(readmeta.getReturnPeriod(con, id))
         
-        colname = 'loss_rp_'+str(return_period)
+        colname = 'loss_rp_'+str(int(return_period))
         rps.append(return_period)
         probs.append(1.0/return_period)
         lossdata = lossdata.rename(
@@ -119,24 +124,29 @@ def PrepareLossForRisk(con, lossids, extensions, hazard):
             i = False
         else:
             prepared_loss = prepared_loss.merge(lossdata, on='Unit_ID')
+            
+            
+            
+            
     modified_loss, cols, probs = predict_loss(
         prepared_loss, rps, probs, extensions, hazard)
-    cost_data, cost_col, earPK = getMaxcost(con, lossids[0])
+    cost_data, cost_col, earPK = getMaxcost(con, lossids_list[0])
     prepared_loss = pd.merge(left=modified_loss, right=cost_data[[earPK, cost_col]], how='outer', left_on=[
                              'Unit_ID'], right_on=[earPK], right_index=False).rename(columns={cost_col: 'MAX_COST'})
-    return modified_loss, cols, probs
+    
+    return prepared_loss, cols, probs
 
 
 def combineLosses(lossess, interacting_hazards, interaction='independent', haz_prob=None):
-    if interaction == 'cascading':
+    if interaction.lower() == 'cascading':
         interaction_function = mul_cascading
-    elif interaction == 'compounding':
+    elif interaction.lower() == 'compounding':
         interaction_function = mul_compounding
-    elif interaction == 'conditional':
+    elif interaction.lower() == 'conditional':
         interaction_function = mul_conditional
-    elif interaction == 'coupled':
+    elif interaction.lower() == 'coupled':
         interaction_function = mul_coupled
-    elif interaction == 'independent':
+    elif interaction.lower() == 'independent':
         interaction_function = mul_independent
     else:
         raise ValueError(
@@ -151,12 +161,14 @@ def combineLosses(lossess, interacting_hazards, interaction='independent', haz_p
         column_multiply = lossess[haz]['cols']
         haz_index = interacting_hazards.index(haz)
         if haz_prob != None:
-            loss[column_multiply] = loss[column_multiply]*haz_prob[haz_index]
+            loss[column_multiply] = loss[column_multiply]*haz_prob
+            # loss[column_multiply] = loss[column_multiply]*haz_prob[haz_index]
         if first:
             all_loss = loss
             first = False
         else:
-            loss = loss.drop(columns=['MAX_COST'])
+            if 'MAX_COST' in loss.columns:
+                loss = loss.drop(columns=['MAX_COST']) 
             all_loss = pd.merge(left=all_loss, right=loss, how='outer', left_on=[
                                 'Unit_ID'], right_on=['Unit_ID'], right_index=False)
 
@@ -232,7 +244,6 @@ def calculateRisk(lossdf, columns, probs):
         xx = row[columns].values.tolist()
         yy = probs
         aal = dutch_method(xx, yy)
-        # print('ear',aal)
         ear_id = row['Unit_ID']
         new_row = {'Unit_ID': ear_id, 'AAL': aal}
         # append row to the dataframe
@@ -263,21 +274,25 @@ def calculateRisk(lossdf, columns, probs):
 #
 
 def computeMulRisk(connstr, groupcombinations, extensions, riskid, **kwargs):
-    print(groupcombinations,"groupcombinations")
-    print(extensions,"extensions")
-    print(riskid,"riskid")
     is_aggregated = kwargs.get('is_aggregated', False)
     onlyaggregated = kwargs.get('only_aggregated', False)
     adminid = kwargs.get('adminunit_id', None)
 
     first_risk = True
     for group in groupcombinations:
+        
         # losscombinations = groupcombinations[group]['lossid']
         # hazardinteractions = groupcombinations[group]['interactions']
         # hazards = groupcombinations[group]['hazards']
         # weights = groupcombinations[group]['weights']
+        
         losscombinations = group['lossIds']
-        hazardinteractions = group['interaction']
+        hazardinteraction = group['interaction']
+        hazardinteractions = []
+        if isinstance(hazardinteraction, str):
+            hazardinteractions.append(hazardinteraction)
+        else:
+            hazardinteractions=hazardinteraction
         hazards = group['hazardType']
         if 'weight' in group.keys():
             weights = group['weight']
@@ -294,6 +309,7 @@ def computeMulRisk(connstr, groupcombinations, extensions, riskid, **kwargs):
             loss_haz = {'normalized_loss': normalized_loss,
                         'cols': cols, 'probs': probs}
             loss_normalization[hazard] = loss_haz
+            
         for interaction in hazardinteractions:
             interacting_hazards = hazards  # hazardinteractions[interaction]
             if (interaction == 'cascading') | (interaction == 'conditional'):
@@ -365,8 +381,9 @@ def computeMulRisk(connstr, groupcombinations, extensions, riskid, **kwargs):
                 final_loss = merged
 
         # prepare information for the calculation of risk
-        probabilities = loss_haz[probs]
-        columns = loss_haz[cols]
+        # probabilities = probs
+        probabilities = loss_haz['probs']
+        columns = loss_haz["cols"]
         columns = [w.replace(Name_hazard, 'combined') for w in columns]
         risk = calculateRisk(final_loss, columns, probabilities)
         if first_risk:
@@ -380,7 +397,7 @@ def computeMulRisk(connstr, groupcombinations, extensions, riskid, **kwargs):
             combined_df = combined_df.drop(["AAL_total", "AAL"], axis=1)
             del totalrisk
             totalrisk = combined_df.rename(columns={"AAL_sum": "AAL_total"})
-    risk = totalrisk.rename(columns={"AAL_total": "AAL"})
+    risk = totalrisk.rename(columns={"AAL_total": "AAL","Unit_ID": "geom_id"})
     metatable = readmeta.readLossMeta(connstr, lossids[0])
     schema = metatable.workspace[0]
     risk['risk_id'] = riskid