e3afcfc51abe305414683c30bc62a3645b4f4273
rabinatwayana <rabinatwayana123@gmail.com>
2023-05-13 11:31:12 +0700

    fix: error in exposure calculation

--- RiskChanges/RiskChangesOps/rasterops.py
+++ RiskChanges/RiskChangesOps/rasterops.py
@@ -3,5 +3,7 @@ from rasterio.windows import from_bounds
 from rasterio.mask import mask
 import rasterio
 def cropraster(raster,geometry):
-    maska,transform=rasterio.mask.mask(raster, geometry, crop=True,nodata=0,all_touched=True)
-    return maska,transform
+    maska,transform=rasterio.mask.mask(raster, geometry, crop=True,nodata=0,all_touched=True,filled=True)
+    image=rasterio.features.rasterize(geometry, out_shape=maska.shape[1:],fill=0,all_touched=True,transform=transform)
+    len_ras=image.sum()
+    return maska,transform,len_ras
