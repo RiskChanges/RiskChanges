2aac3177212d6f967d23bb2f0687369a6a502085
rabinatwayana <rabinatwayana123@gmail.com>
2023-05-17 19:58:48 +0700

    fix: spatial units that are not exposed are also not displayed

--- RiskChanges/RiskChangesOps/rasterops.py
+++ RiskChanges/RiskChangesOps/rasterops.py
@@ -3,7 +3,11 @@ from rasterio.windows import from_bounds
 from rasterio.mask import mask
 import rasterio
 def cropraster(raster,geometry):
-    maska,transform=rasterio.mask.mask(raster, geometry, crop=True,nodata=0,all_touched=True,filled=True)
-    image=rasterio.features.rasterize(geometry, out_shape=maska.shape[1:],fill=0,all_touched=True,transform=transform)
-    len_ras=image.sum()
-    return maska,transform,len_ras
+    try:
+        maska,transform=rasterio.mask.mask(raster, geometry, crop=True,nodata=0,all_touched=True,filled=True)
+        image=rasterio.features.rasterize(geometry, out_shape=maska.shape[1:],fill=0,all_touched=True,transform=transform)
+        len_ras=image.sum()
+        return maska,transform,len_ras
+    except Exception as e:
+        print(f"error from crop raster: {str(e)}")
+        return None
