2c416584e31090bda11d5df309e0a671d67e9cd9
rabinatwayana <rabinatwayana123@gmail.com>
2023-08-03 16:28:10 +0700

    feat: add all missing hazard class column in getSummary function and also solve its ordering

--- RiskChanges/getExposure.py
+++ RiskChanges/getExposure.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__file__)
 #! create function for similar calculation
 
 def add_hazard_class(exposure,min_thresholds,classificationScheme):
+    # Change the classes to the user defined class
     try:
         convert_dict = {}
         for i, val in enumerate(min_thresholds):
@@ -26,10 +27,11 @@ def add_hazard_class(exposure,min_thresholds,classificationScheme):
             if (val == min_thresholds[-1]):
                 exposure['class'] = np.where(
                     exposure['class'] >= i+1, i+1, exposure['class'])
+        logger.info(f"{convert_dict} convert_dict")
         exposure['class'].replace(convert_dict, inplace=True)
-        return exposure
+        return True,exposure,convert_dict
     except Exception as e:
-        return {'error':str(e)}      
+        return False,str(e),None    
 
 def getSummary(con, exposureid, column='areaOrLen', agg=False):
     try:
@@ -40,22 +42,18 @@ def getSummary(con, exposureid, column='areaOrLen', agg=False):
         exposure = readvector.prepareExposureForLoss(con, exposureid)
         hazid = metadata['hazid']
         classificationScheme = readmeta.classificationscheme(con, hazid)
+        logger.info(f"{classificationScheme} classificationScheme")
         thresholds=classificationScheme['val1'].unique()
         thresholds.sort()
         min_thresholds=[float(val) for val in thresholds]
         type_col = metadata["TypeColumn"]
-        # base = float(metadata["base"])
-        # maxval = float(metadata["threshold"])
-        # stepsize = float(metadata["stepsize"])
-        # min_thresholds = np.arange(
-        #     start=base, stop=maxval, step=stepsize).tolist()
-        
-        # Change the classes to the user defined class
-        add_hazard_class_result=add_hazard_class(exposure,min_thresholds,classificationScheme)
-        if isinstance(add_hazard_class_result,dict):
-            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result['error']}")
-        else:
+        response,add_hazard_class_result,hazard_class_dict=add_hazard_class(exposure,min_thresholds,classificationScheme)
+        if response:
             exposure=add_hazard_class_result
+        else:
+            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result}")
+            
+        logger.info(f"{exposure.columns} column after add_hazard_class_result")
         # exposure['class'].replace(convert_dict, inplace=True)
         
         # if column is count just count the number of feature exposed
@@ -67,6 +65,7 @@ def getSummary(con, exposureid, column='areaOrLen', agg=False):
                                     columns=["class"], aggfunc=np.sum, fill_value=0)
             summary = summary.reset_index()
             summary = summary.rename(columns={type_col: "Ear Class"})
+            
         else:
             summary = pd.pivot_table(exposure, values=column, index=[type_col, 'admin_id'],
                                     columns=["class"], aggfunc=np.sum, fill_value=0)
@@ -77,6 +76,16 @@ def getSummary(con, exposureid, column='areaOrLen', agg=False):
         #to drop column 0.0 if exists
         if 0.0 in summary.columns:
             summary = summary.drop(0.0, axis=1)
+        #Add missing hazard class in table
+        for values in hazard_class_dict.values():
+            if values not in summary.columns:
+                summary[values]=0
+                
+        other_columns = [col for col in summary.columns if col not in hazard_class_dict.values()]
+        desired_columns_order=other_columns+list(hazard_class_dict.values())
+        # Step 5: Reorder the columns using reindex with the desired_order followed by the remaining columns
+        summary = summary.reindex(columns=desired_columns_order)
+        
         return summary
     except Exception as e:
         raise Exception(f"Error in getSummary {str(e)}")
@@ -98,19 +107,12 @@ def getShapefile(con, exposureid, column='areaOrLen', agg=False):
         adminid = metadata['adminid']
         earpk = metadata["earPK"]
         earid = metadata['earID']
-        # maxval = float(metadata["threshold"])
-        # base = float(metadata["base"])
-        # stepsize = float(metadata["stepsize"])
-        # min_thresholds = np.arange(
-        #     start=base, stop=maxval+1, step=stepsize).tolist()
-        add_hazard_class_result=add_hazard_class(exposure,min_thresholds,classificationScheme)
-        if isinstance(add_hazard_class_result,dict):
-            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result['error']}")
-        else:
+        response,add_hazard_class_result,hazard_class_dict==add_hazard_class(exposure,min_thresholds,classificationScheme)
+        if response:
             exposure=add_hazard_class_result
-        # Change the classes to the user defined class
-        # exposure["class"].replace(convert_dict, inplace=True)
-
+        else:
+            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result}")
+        
         # if column is count just count the number of feature exposed
         if column == 'count':
             exposure[column] = 1
@@ -134,6 +136,14 @@ def getShapefile(con, exposureid, column='areaOrLen', agg=False):
         #to drop column 0.0 if exists
         if 0.0 in summary.columns:
             summary = summary.drop(0.0, axis=1)
+        #Add missing hazard class in table
+        for values in hazard_class_dict.values():
+            if values not in summary.columns:
+                summary[values]=0
+        other_columns = [col for col in summary.columns if col not in hazard_class_dict.values()]
+        desired_columns_order=other_columns+list(hazard_class_dict.values())
+        # Step 5: Reorder the columns using reindex with the desired_order followed by the remaining columns
+        summary = summary.reindex(columns=desired_columns_order)
         return summary
     except Exception as e:
         raise Exception(f"Error in getSummary {str(e)}")
@@ -156,12 +166,6 @@ def getSummaryRel(con, exposureid, column='areaOrLen', agg=False):
         costcol = metadata['costColumn']
         popcol = metadata['populColumn']
         type_col = metadata["TypeColumn"]
-        # base = float(metadata["base"])
-        # maxval = float(metadata["threshold"])
-        # stepsize = float(metadata["stepsize"])
-        # min_thresholds = np.arange(
-        #     start=base, stop=maxval+1, step=stepsize).tolist()
-        # convert_dict = {}
         if column == 'population_exposure':
             aggcolumn = popcol
         elif column == 'value_exposure':
@@ -169,13 +173,11 @@ def getSummaryRel(con, exposureid, column='areaOrLen', agg=False):
         else:
             aggcolumn = 'areaOrLength'
         
-        add_hazard_class_result=add_hazard_class(exposure,min_thresholds,classificationScheme)
-        if isinstance(add_hazard_class_result,dict):
-            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result['error']}")
-        else:
+        response,add_hazard_class_result,hazard_class_dict=add_hazard_class(exposure,min_thresholds,classificationScheme)
+        if response:
             exposure=add_hazard_class_result
-        # Change the classes to the user defined class
-        # exposure['class'].replace(convert_dict, inplace=True)
+        else:
+            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result}")
         # if column is count just count the number of feature exposed
         if column == 'count':
             exposure[column] = 1
@@ -206,6 +208,17 @@ def getSummaryRel(con, exposureid, column='areaOrLen', agg=False):
         #to drop column 0.0 if exists
         if 0.0 in summary.columns:
             summary = summary.drop(0.0, axis=1)
+        if 'areaOrLength' in summary.columns:
+            summary = summary.drop('areaOrLength', axis=1)
+            
+        #Add missing hazard class in table
+        for values in hazard_class_dict.values():
+            if values not in summary.columns:
+                summary[values]=0
+        other_columns = [col for col in summary.columns if col not in hazard_class_dict.values()]
+        desired_columns_order=other_columns+list(hazard_class_dict.values())
+        # Step 5: Reorder the columns using reindex with the desired_order followed by the remaining columns
+        summary = summary.reindex(columns=desired_columns_order)
         return summary
     except Exception as e:
         raise Exception(f"Error in getSummary {str(e)}")
@@ -227,12 +240,6 @@ def getShapefileRel(con, exposureid, column='areaOrLen', agg=False):
         adminid = metadata['adminid']
         earpk = metadata["earPK"]
         earid = metadata['earID']
-        # base = float(metadata["base"])
-        # maxval = float(metadata["threshold"])
-        # stepsize = float(metadata["stepsize"])
-        # min_thresholds = np.arange(
-        #     start=base, stop=maxval+1, step=stepsize).tolist()
-        # convert_dict = {}
         costcol = metadata['costColumn']
         popcol = metadata['populColumn']
         if column == 'population_exposure':
@@ -241,14 +248,15 @@ def getShapefileRel(con, exposureid, column='areaOrLen', agg=False):
             aggcolumn = costcol
         else:
             aggcolumn = 'areaOrLength'
-        add_hazard_class_result=add_hazard_class(exposure,min_thresholds,classificationScheme)
+        response,add_hazard_class_result,hazard_class_dict==add_hazard_class(exposure,min_thresholds,classificationScheme)
+        if response:
+            exposure=add_hazard_class_result
+        else:
+            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result}")
         if isinstance(add_hazard_class_result,dict):
             raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result['error']}")
         else:
             exposure=add_hazard_class_result
-        # Change the classes to the user defined class
-        # exposure["class"].replace(convert_dict, inplace=True)
-
         # if column is count just count the number of feature exposed
         if column == 'count':
             exposure[column] = 1
@@ -279,6 +287,16 @@ def getShapefileRel(con, exposureid, column='areaOrLen', agg=False):
         #to drop column 0.0 if exists
         if 0.0 in summary.columns:
             summary = summary.drop(0.0, axis=1)
+        if 'areaOrLength' in summary.columns:
+            summary = summary.drop('areaOrLength', axis=1)
+        #Add missing hazard class in table
+        for values in hazard_class_dict.values():
+            if values not in summary.columns:
+                summary[values]=0
+        other_columns = [col for col in summary.columns if col not in hazard_class_dict.values()]
+        desired_columns_order=other_columns+list(hazard_class_dict.values())
+        # Step 5: Reorder the columns using reindex with the desired_order followed by the remaining columns
+        summary = summary.reindex(columns=desired_columns_order)
         return summary
     except Exception as e:
         raise Exception(f"Error in getSummary {str(e)}")
