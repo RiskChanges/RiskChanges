5624d1cedf4992bbfd41e938cec00917464d0e6f
rabinatwayana <rabinatwayana123@gmail.com>
2023-09-20 13:42:43 +0700

    feat: handle no_data and nan values in raster reclassify function

--- RiskChanges/RiskChangesOps/readraster.py
+++ RiskChanges/RiskChangesOps/readraster.py
@@ -3,29 +3,28 @@ import rasterio
 import numpy as np
 import os
 
-
 def reclassify(in_image, out_image, base, stepsize, maxval):
     input_image = rasterio.open(in_image)
     intensity_data = input_image.read(1)
-
+    has_nodata = np.isnan(intensity_data).any()
+    if has_nodata:
+        intensity_data = np.nan_to_num(intensity_data, nan=0.0)
+    nodata = input_image.nodata #this will handle value with -999
+    intensity_data[intensity_data==nodata]=0.0
     prev = base
     thresholds = np.arange(start=base, stop=maxval+1, step=stepsize).tolist()
-    intensity_data[intensity_data < base] = input_image.nodata
+    intensity_data[intensity_data < base] = 0.0
+    # intensity_data[intensity_data < base] = input_image.nodata
     intensity_data_classified = np.copy(intensity_data)
-
     for i, threshold in enumerate(thresholds):
         #mean=intensity_data[((intensity_data<threshold) & (intensity_data>=prev))].mean()
-
         intensity_data_classified[(
             (intensity_data < threshold) & (intensity_data >= prev))] = i
-
         prev = threshold
-
         # if it is the last value, need to assign the max class for all result
         if threshold == thresholds[-1]:
             intensity_data_classified[(
                 intensity_data >= thresholds[-1])] = i
-
     with rasterio.Env():
         profile = input_image.profile
         with rasterio.open(out_image, 'w', **profile) as dst:
