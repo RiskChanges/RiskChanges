f5f0368fb3cf73d62b3edcfdca8f4334702f9c9c
rabinatwayana <rabinatwayana123@gmail.com>
2023-10-03 17:16:27 +0700

    handle exception and fix error related to defining geom field

--- RiskChanges/load_DB.py
+++ RiskChanges/load_DB.py
@@ -2,28 +2,29 @@ import geopandas as gpd
 from sqlalchemy.engine import create_engine
 from geoalchemy2 import Geometry, WKTElement
 
-
 def load_db(query, source_connection, destination_connection, destination_table, destination_schema, index):
-
-    df = gpd.read_postgis(query, source_connection)
-
-    crs = df.crs
+    #TODO if df.crs is not projected, convert it
     try:
-        epsg = crs.to_epsg()
-
-    except:
-        print('Warning! Coordinate system manually assigned to 4326. This might affect on visualization of data.')
-        epsg = 4326
-
-    df['geom'] = df['geom'].apply(
-        lambda x: WKTElement(x.wkt, srid=epsg))
-
-    df[index] = df.index
-    print(df.head())
-
-    engine = create_engine(destination_connection)
-
-    df.to_sql(destination_table, engine, schema=destination_schema, if_exists='replace', index=False,
-              dtype={'geom': Geometry('Geometry', srid=epsg)})
-
-    engine.dispose()
+        try:
+            df = gpd.read_postgis(query, source_connection,geom_col='geometry')
+        except:
+            df = gpd.read_postgis(query, source_connection,geom_col='geom')
+            df=df.rename( columns={"geom":"geometry"})
+        if df.empty:
+            return False, "Geometry column must be named with geom or geometry"
+        crs = df.crs
+        try:
+            epsg = crs.to_epsg()
+        except:
+            print('Warning! Coordinate system manually assigned to 4326. This might affect on visualization of data.')
+            epsg = 4326
+        df['geometry'] = df['geometry'].apply(
+            lambda x: WKTElement(x.wkt, srid=epsg))
+        df[index] = df.index
+        engine = create_engine(destination_connection)
+        df.to_sql(destination_table, engine, schema=destination_schema, if_exists='replace', index=False,
+                dtype={'geometry': Geometry(geometry_type='GEOMETRY', srid=epsg)})
+        engine.dispose()
+        return True, "success"
+    except Exception as e:
+        return False, str(e)
