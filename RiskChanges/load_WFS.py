64b3f68fd2a65dda0432ac510c9b4f719c5c076a
rabinatwayana <rabinatwayana123@gmail.com>
2023-10-03 17:14:59 +0700

    refactor: handle exception in load_WFS function

--- RiskChanges/load_WFS.py
+++ RiskChanges/load_WFS.py
@@ -6,25 +6,28 @@ from owslib.wfs import WebFeatureService
 
 
 def LoadWFS(wfsURL, layer_name, connstr, layer_name_db, index, schema):
+    #TODO if gdf is not projected, convert it
+    try:
+        params = dict(service='WFS', version="1.0.0", request='GetFeature',
+                    typeName=layer_name, outputFormat='application/json')
+        u = requests.get(wfsURL, params)
+        data_url = u.url
+        
+        # Read data from URL
+        gdf = gpd.read_file(data_url)
+        gdf = gdf[gdf.is_valid]
+        gdf[index] = gdf.index
 
-    params = dict(service='WFS', version="1.0.0", request='GetFeature',
-                  typeName=layer_name, outputFormat='application/json')
-
-    u = requests.get(wfsURL, params)
-    data_url = u.url
-
-    # Read data from URL
-    gdf = gpd.read_file(data_url)
-    gdf = gdf[gdf.is_valid]
-    gdf[index] = gdf.index
-
-    # Creating SQLAlchemy's engine to use
-    engine = create_engine(connstr)
-    gdf.to_postgis(name=layer_name_db, con=engine,
-                   schema=schema, if_exists='replace')
-
-    # close engine
-    engine.dispose()
+        # Creating SQLAlchemy's engine to use
+        engine = create_engine(connstr)
+        gdf.to_postgis(name=layer_name_db, con=engine,
+                    schema=schema, if_exists='replace')
+        
+        # close engine
+        engine.dispose()
+        return True, "successful"
+    except Exception as e:
+        return False, str(e)
 
 
 # LoadWFS(wfsURL="http://geo.stat.fi/geoserver/vaestoruutu/wfs",layer_name='vaestoruutu:vaki2005_1km_kp',connstr='postgresql://postgres:puntu@localhost:5432/postgres',lyrName='Tekson_WFS',schema='tekson')
@@ -32,13 +35,18 @@ def LoadWFS(wfsURL, layer_name, connstr, layer_name_db, index, schema):
 # LoadWFS(r'http://tajirisk.ait.ac.th:8080/geoserver/ows',
 #         'tajikistan:region', 'postgresql://postgres:gicait123@203.159.29.45:5432/sdssv2', 'kamal', 'geoinformatics_center')
 
-
+# not in use in riskchanges backend
 def test_WFS(wfs_url, layer_name):
-    wfs11 = WebFeatureService(url=wfs_url, version='1.1.0')
-    wfs_layers = list(wfs11.contents)
+    try:
+        wfs11 = WebFeatureService(url=wfs_url, version='1.1.0')
+        wfs_layers = list(wfs11.contents)
 
-    if layer_name in wfs_layers:
-        return layer_name
+        if layer_name in wfs_layers:
+            return True, layer_name
 
-    else:
-        return "Layer not available. Available layers are: " + wfs_layers
+        else:
+            return False , "Layer not available. Available layers are: " + wfs_layers
+    except Exception as e:
+        return False , str(e)
+        
+        
