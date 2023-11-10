feat: fix exposure calculation using susceptible hazard classes data

RiskChanges/RiskChangesOps/readraster.py
@@ -14,6 +14,7 @@ def reclassify(in_image, out_image, base, stepsize, maxval):
     prev = base
     thresholds = np.arange(start=base, stop=maxval+1, step=stepsize).tolist()
     intensity_data[intensity_data < base] = 0.0
+    
     # intensity_data[intensity_data < base] = input_image.nodata
     intensity_data_classified = np.copy(intensity_data)
     for i, threshold in enumerate(thresholds):
@@ -21,6 +22,7 @@ def reclassify(in_image, out_image, base, stepsize, maxval):
         intensity_data_classified[(
             (intensity_data < threshold) & (intensity_data >= prev))] = i
         prev = threshold
+        
         # if it is the last value, need to assign the max class for all result
         if threshold == thresholds[-1]:
             intensity_data_classified[(
@@ -32,6 +34,22 @@ def reclassify(in_image, out_image, base, stepsize, maxval):
         # dst = None
     # input_image = None
     input_image.close()
+    
+def refactor(in_image, out_image, base, threshold):
+    src = rasterio.open(in_image)
+    src_data = src.read(1)
+    has_nodata = np.isnan(src_data).any()
+    if has_nodata:
+        src_data = np.nan_to_num(src_data, nan=0.0)
+    nodata = src.nodata #this will handle value with -999
+    src_data[src_data==nodata]=0.0
+    src_data[src_data < base] = 0.0
+    src_data[src_data > threshold] = 0.0
+    with rasterio.Env():
+        profile = src.profile
+        with rasterio.open(out_image, 'w', **profile) as dst:
+            dst.write(src_data, 1)
+    src.close()
 
 
 def ClassifyHazard(hazard_file, base, stepsize, threshold):
@@ -44,6 +62,16 @@ def ClassifyHazard(hazard_file, base, stepsize, threshold):
         reclassify(infile, outfile, base, stepsize, threshold)
     return outfile
 
+def RefactorClassifiedHazard(hazard_file, base, threshold):
+    file_name, file_extension = os.path.splitext(hazard_file)
+    outfile = hazard_file.replace(file_extension, f"_reclassified{file_extension}")
+    # if os.path.isfile(outfile):
+    #     pass
+    # else:
+    refactor(hazard_file, outfile, base, threshold)
+    return outfile
+
+
 
 def readhaz(connstr, hazid, haz_file):
     hazard_metadata = readmeta.hazmeta(connstr, hazid)
@@ -52,14 +80,17 @@ def readhaz(connstr, hazid, haz_file):
     hazfile = hazard_metadata.file[0]
     threshold = hazard_metadata.threshold_val[0] or hazard_metadata.raster_max_value[0]
     intensity_type = hazard_metadata.intensity[0]
+    unit=hazard_metadata.unit[0]
+    
     if haz_file:
         hazfile = haz_file
 
-    if intensity_type == 'Susceptibility':
-        outfile = hazfile
+    if intensity_type == 'Susceptibility' and unit== "classes":
+        # outfile = hazfile
+        outfile = RefactorClassifiedHazard(hazfile, base, threshold)
     else:
         outfile = ClassifyHazard(hazfile, base, step_size, threshold)
-
+        
     src = rasterio.open(outfile)
     return src
 