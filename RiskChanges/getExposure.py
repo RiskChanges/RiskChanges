12057d97b8b796dae9294df09944a97f91ba1a7a
rabinatwayana <rabinatwayana123@gmail.com>
2024-04-11 14:38:10 +0700

    fix: issue in exposure summary of raster datasets

--- RiskChanges/getExposure.py
+++ RiskChanges/getExposure.py
@@ -4,8 +4,8 @@ import numpy as np
 from .RiskChangesOps.readvulnerability import readIntVuln, readSusVuln
 from .RiskChangesOps import readmeta, readvector, writevector, AggregateData as aggregator
 
-# import logging
-# logger = logging.getLogger(__file__)
+import logging
+logger = logging.getLogger(__file__)
 #! create function for similar calculation
 
 def add_hazard_class(exposure,min_thresholds,classificationScheme,haz_class_field="class"):
@@ -28,12 +28,14 @@ def add_hazard_class(exposure,min_thresholds,classificationScheme,haz_class_fiel
                     exposure[haz_class_field] >= i+1, i+1, exposure[haz_class_field])
                 
         exposure[haz_class_field].replace(convert_dict, inplace=True)
+        logger.info(f'{convert_dict} convert_dictconvert_dict ')
         return True,exposure,convert_dict
     except Exception as e:
         return False,str(e),None    
 
 def getSummary(con, exposureid, column='areaOrLen', aggregation=None):
     try:
+        
         if column not in ['areaOrLen', 'value_exposure', 'population_exposure', 'count']:
             raise ValueError(
                 "column: status must be one of areaOrLen, value_exposure or population_exposure")
@@ -105,7 +107,7 @@ def getShapefile(con, exposureid, column='areaOrLen', aggregation=None):
     try:
         if column not in ['areaOrLen', 'value_exposure', 'population_exposure', 'count']:
             raise ValueError(
-                "column: status must be one of areaOrLen, value_exposure or population_exposure")
+                "column value must be one of areaOrLen, value_exposure or population_exposure")
         metadata = readmeta.computeloss_meta(con, exposureid)
         exposure = readvector.prepareExposureForLoss(con, exposureid)
         hazid = metadata["hazid"]
@@ -168,7 +170,7 @@ def getSummaryRel(con, exposureid, column='areaOrLen', aggregation=None):
     try:
         if column not in ['areaOrLen', 'value_exposure', 'population_exposure', 'count']:
             raise ValueError(
-                "column: status must be one of areaOrLen, value_exposure or population_exposure")
+                "column value must be one of areaOrLen, value_exposure or population_exposure")
         metadata = readmeta.computeloss_meta(con, exposureid)
         exposure = readvector.prepareExposureForLoss(con, exposureid)
         hazid = metadata['hazid']
@@ -354,9 +356,9 @@ def getShapefileRel(con, exposureid, column='areaOrLen', aggregation=None):
         raise Exception(f"Error in getSummary {str(e)}")
 
 def getGriddedExposureSummary(con, exposure_id, column='total_area_exposed', aggregation=None):
-    # def getGriddedExposureSummary(con, exposure_id, column='total_area_exposed', agg=False,agg_func=False):
     '''
     aggregation: individual_ear_class,admin_wise,ear_admin_wise
+    add_hazard_class is commented out since the class_name is saved with value directly not id
     '''
     try:
         if column not in ['total_pixel_exposed', 'total_area_exposed', 'relative_exposed']:
@@ -378,12 +380,13 @@ def getGriddedExposureSummary(con, exposure_id, column='total_area_exposed', agg
         thresholds.sort()
         min_thresholds=[float(val) for val in thresholds]
         type_col = "ear_name" #metadata["TypeColumn"]
-        response,add_hazard_class_result,hazard_class_dict=add_hazard_class(exposure,min_thresholds,classificationScheme,"hazard_name")
         
-        if response:
-            exposure=add_hazard_class_result
-        else:
-            raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result}")
+        # response,add_hazard_class_result,hazard_class_dict=add_hazard_class(exposure,min_thresholds,classificationScheme,"hazard_name")
+        
+        # if response:
+            # exposure=add_hazard_class_result
+        # else:
+            # raise Exception(f"Error in add_hazard_class_result: {add_hazard_class_result}")
         if aggregation=='ear_admin_wise':
             # if agg:
             summary = pd.pivot_table(exposure, values=column, index=[type_col, 'admin_id'],
@@ -406,31 +409,22 @@ def getGriddedExposureSummary(con, exposure_id, column='total_area_exposed', agg
             summary = summary.reset_index()
             summary = summary.rename(
                 columns={type_col: "Ear Class"})
-            # summary = pd.pivot_table(exposure, values=column, index=[type_col],
-            #                         columns=["hazard_name"], fill_value=0) #aggfunc=np.sum, 
-            # summary = summary.reset_index()
-            # summary = summary.rename(columns={type_col: "Ear Class"})
-            
-        # elif aggregation=='individual_ear_class':
-        # # elif agg_func:
-        #     summary = pd.pivot_table(exposure, values=column, index=[type_col],
-        #                             columns=["hazard_name"], aggfunc=np.sum, fill_value=0) #aggfunc=np.sum,
-        #     summary = summary.reset_index()
-        #     summary = summary.rename(
-        #         columns={type_col: "Ear Class"})
             
         summary = summary.fillna(0)
         #to drop column 0.0 if exists
         if 0.0 in summary.columns:
             summary = summary.drop(0.0, axis=1)
-        #Add missing hazard class in table
-        for values in hazard_class_dict.values():
-            if values not in summary.columns:
-                summary[values]=0
-        other_columns = [col for col in summary.columns if col not in hazard_class_dict.values()]
-        desired_columns_order=other_columns+list(hazard_class_dict.values())
-        # Step 5: Reorder the columns using reindex with the desired_order followed by the remaining columns
-        summary = summary.reindex(columns=desired_columns_order)
+            
+        # #Add missing hazard class in table
+        # for values in hazard_class_dict.values():
+        #     if values not in summary.columns:
+        #         summary[values]=0
+                
+        # other_columns = [col for col in summary.columns if col not in hazard_class_dict.values()]
+        # desired_columns_order=other_columns+list(hazard_class_dict.values())
+        # # Step 5: Reorder the columns using reindex with the desired_order followed by the remaining columns
+        # summary = summary.reindex(columns=desired_columns_order)
+        
         return True, summary
     except Exception as e:
         return False, str(e)
